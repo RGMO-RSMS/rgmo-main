2388cda3fc7e2b3c0f7bd2ab3ee0484b7b431a26
MCSaders <mindcodesaders@gmail.com>
2023-11-14 07:34:56 +0800

    about

---
+++ about/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>About</title>
+    <?php require_once __DIR__ . '/../components/link.html'; ?>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="row justify-content-center">
+        <div class="card border border-primary m-4 col-10">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-sm-1">
+                        <a href="#" onclick="history.back()"><i class="fas fa-arrow-left" style="color: primary;"></i></a>
+                    </div>
+                    <div class="col-sm-10"><h4>About</h4></div>
+                </div>
+            </div>
+            <div class="card-body">
+              ahgdh
+            </div>
+        </div>
+        <!-- /.card -->
+    </div>
+    <!-- /.row -->
+
+<?php require_once __DIR__ . '/../components/script.html'; ?>
+
+<script>
+
+
+</script>
+
+</body>
+</html>
