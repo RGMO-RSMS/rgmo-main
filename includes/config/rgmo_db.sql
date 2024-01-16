c23924ba70671e8da04664384f2974e9d4446ce3
daywalker5937 <rgmdmn@gmail.com>
2024-01-16 13:24:02 +0800

    database with actual picture of services

--- includes/config/rgmo_db.sql
+++ includes/config/rgmo_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2023 at 12:29 AM
+-- Generation Time: Jan 16, 2024 at 06:23 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -40,7 +40,8 @@ CREATE TABLE `tbl_client_form` (
 --
 
 INSERT INTO `tbl_client_form` (`id`, `client_id`, `status`, `date`, `service_id`) VALUES
-(18, 10, 'Client Payment', '2023-11-12', 4);
+(18, 10, 'Client Payment', '2023-11-12', 4),
+(19, 22, 'Client Payment', '2023-11-15', 1);
 
 -- --------------------------------------------------------
 
@@ -83,7 +84,8 @@ CREATE TABLE `tbl_payments` (
 --
 
 INSERT INTO `tbl_payments` (`payment_id`, `service_price`, `total_paid`, `due_date`, `form_id`) VALUES
-(14, 300000, 2000, '0000-00-00', 18);
+(14, 300000, 2000, '0000-00-00', 18),
+(15, 500000, 5000, '0000-00-00', 19);
 
 -- --------------------------------------------------------
 
@@ -105,7 +107,8 @@ CREATE TABLE `tbl_payment_logs` (
 --
 
 INSERT INTO `tbl_payment_logs` (`logs_id`, `client_id`, `payment`, `payment_balance`, `payment_id`, `log_date`) VALUES
-(18, 10, 2000, 298000, 14, '2023-11-12');
+(18, 10, 2000, 298000, 14, '2023-11-12'),
+(19, 22, 5000, 495000, 15, '2023-11-15');
 
 -- --------------------------------------------------------
 
@@ -175,7 +178,7 @@ CREATE TABLE `tbl_type_of_service` (
 --
 
 INSERT INTO `tbl_type_of_service` (`type_id`, `type_name`, `location`, `price`, `description`, `availability_status`, `service_image`, `service_id`) VALUES
-(1, 'Isabela Farm', 'Isabela', 500000, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry/\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has s', 'yes', 'farm1.jpg', 1),
+(1, 'Isabela Farm', 'Isabela', 500000, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry/\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has s', 'no', 'farm1.jpg', 1),
 (2, 'Cagayan Farm', 'Cagayan', 2000000, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry/\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has s', 'yes', 'farm2.jpg', 1),
 (3, 'Ilocos Farm', 'Ilocos Norte', 1200000, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry/\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has s', 'yes', 'farm3.jpg', 1),
 (4, 'Camella Housing', 'Cavite', 300000, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry/\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has s', 'no', 'housing1.jpg', 2),
@@ -185,8 +188,38 @@ INSERT INTO `tbl_type_of_service` (`type_id`, `type_name`, `location`, `price`, 
 (8, 'Coffee Stall', 'Manila', 5000, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry/\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has s', 'yes', 'stall1.jpg', 3),
 (9, 'Pizza Stall', 'Makati', 4000, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry/\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has s', 'yes', 'stall2.jpg', 3),
 (10, 'Isabela Rice Field', 'Isabela', 800000, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry/\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has s', 'yes', 'palay1.jpg', 4),
-(11, 'Cagayan Rice Field', 'Cagayan', 900000, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry/\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has s', 'yes', 'palay2.jpg', 4),
-(12, 'Transylvania Room', 'Cubao', 500000, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry/\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has s', 'yes', 'luxury1.jpg', 5);
+(11, 'Cagayan Rice Field', 'Cagayan', 900000, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry/\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has s', 'yes', 'palay2.jpg', 1),
+(12, 'Biazon Hostel Building', 'Isabela', 50000, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry/\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has s', 'yes', 'BH building.jpg', 5),
+(20, 'Isabela Canteen 1', 'Isabela State University', 10000, 'Canteen for Students', 'yes', 'canteen1.jpg', 3),
+(21, 'Isabela Canteen 2', 'Isabela State University', 10000, 'Canteen for Students', 'yes', 'canteen2.jpg', 3),
+(22, 'Isabela Canteen 3', 'Isabela State University', 10000, 'Canteen for Students', 'yes', 'canteen3.jpg', 3),
+(23, 'Isabela Canteen 4', 'Isabela State University', 10000, 'Canteen for Students', 'yes', 'canteen4.jpg', 3),
+(24, 'Isabela Canteen 5', 'Isabela State University', 10000, 'Canteen for Students', 'yes', 'canteen5.jpg', 3),
+(25, 'Isabela Canteen 6', 'Isabela State University', 10000, 'Canteen for Students', 'yes', 'canteen6.jpg', 3),
+(26, 'Isabela Canteen 7', 'Isabela State University', 10000, 'Canteen for Students', 'yes', 'canteen7.jpg', 3),
+(27, 'Isabela Canteen 8', 'Isabela State University', 10000, 'Canteen for Students', 'yes', 'canteen8.jpg', 3),
+(28, 'Isabela Canteen 9', 'Isabela State University', 10000, 'Canteen for Students', 'yes', 'canteen9.jpg', 3),
+(29, 'Isabela Canteen 10', 'Isabela State University', 10000, 'Canteen for Students', 'yes', 'canteen10.jpg', 3),
+(30, 'Staff Housing 1', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing1.jpg', 2),
+(31, 'Staff Housing 2', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing2.jpg', 2),
+(32, 'Staff Housing 3', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing3.jpg', 2),
+(33, 'Staff Housing 4', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing4.jpg', 2),
+(34, 'Staff Housing 5', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing5.jpg', 2),
+(35, 'Staff Housing 6', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing6.jpg', 2),
+(36, 'Staff Housing 7', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing7.jpg', 2),
+(37, 'Staff Housing 8', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing8.jpg', 2),
+(38, 'Staff Housing 9', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing9.jpg', 2),
+(39, 'Staff Housing 10', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing10.jpg', 2),
+(40, 'Staff Housing 11', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing11.jpg', 2),
+(41, 'Staff Housing 12', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing12.jpg', 2),
+(42, 'Staff Housing 13', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing13.jpg', 2),
+(43, 'Staff Housing 14', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing14.jpg', 2),
+(44, 'Staff Housing 15', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing15.jpg', 2),
+(45, 'Staff Housing 16', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing16.jpg', 2),
+(46, 'Staff Housing 17', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing17.jpg', 2),
+(47, 'Staff Housing 18', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing18.jpg', 2),
+(48, 'Staff Housing 19', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing19.jpg', 2),
+(49, 'Staff Housing 20', 'Isabela State University', 10000, 'Housing for Staffs', 'yes', 'staffhousing20.jpg', 2);
 
 -- --------------------------------------------------------
 
@@ -214,7 +247,6 @@ INSERT INTO `tbl_user_info` (`user_id`, `first_name`, `last_name`, `middle_name`
 (10, 'Mary Grace', 'Ortega', 'Waiñu', 'Isabela', '09898484684', 'Female', '302253-Nezuko-Cute-Kimetsu-no-Yaiba-4K.jpg', 'Single'),
 (15, 'Jenny', 'Kim', 'Klein', 'South Korea', '09849849878', 'Female', '309932978_1112414036315316_7836836705751840661_n.jpg', 'Single'),
 (16, 'Admin', 'Account', 'User', 'Isabela', '09589684526', 'Male', '', 'Single'),
-(17, 'Lisa', 'Manoban', 'La', 'South Korea', '09846489448', '', '', 'Single'),
 (18, 'Jose', 'Rizal', 'Protacio', 'Cavite', '09848998648', 'Male', 'Igloo_outside.jpg', 'Divorced'),
 (20, 'Juan', 'Dela Cruz', 'De', 'Manila', '09549848488', 'Male', 'sample 1.png', 'Single'),
 (21, 'Gorgonio', 'Magalpoc', 'Palo', 'Intramuros', '09849848978', 'Male', 'sample 6.png', 'Married'),
@@ -239,9 +271,8 @@ CREATE TABLE `tbl_user_login` (
 
 INSERT INTO `tbl_user_login` (`email`, `password`, `user_id`) VALUES
 ('marygrace@gmail.com', '$2y$10$QMMTVXNn1SdEs4UMnj.Z9O.B3pu3NNeRMrwh5rE7wvqa/uxGARzKa', 10),
-('jenny@gmail.com', '$2y$10$xhH4l8MxAdoXbkdbKH/luOt1gy.OSkASfwpC0a3iBRcQwSdFtA2u6', 15),
+('jennys@gmail.com', '$2y$10$xhH4l8MxAdoXbkdbKH/luOt1gy.OSkASfwpC0a3iBRcQwSdFtA2u6', 15),
 ('admin@gmail.com', '$2y$10$KmLrgQ6MHq5shnzDlsqWduZI7P2vTDhXIoCh0igZVH8FwL9fJJXeO', 16),
-('lisa@gmail.com', '', 17),
 ('jose@gmail.com', '$2y$10$LSfjCXU8gwTD0w2xv8x7I.veZ1ItxV/Yvu9u0zgFlXUSeZ.Lv/7Gy', 18),
 ('juan@gmail.com', '$2y$10$k7SbwSVvLiPIJ4OuMMh20uSZ5WEC84qktE/CYleqkSqhuCrHgWME.', 20),
 ('edwardsarabia.11@gmail.com', '$2y$10$WqEkdP5iNQkhJlg8mHLcNupTUTqxmaE7LZ/itz12H8tjaUGlIb93m', 21),
@@ -333,7 +364,7 @@ ALTER TABLE `tbl_user_info`
 -- AUTO_INCREMENT for table `tbl_client_form`
 --
 ALTER TABLE `tbl_client_form`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `tbl_list_of_service`
@@ -345,13 +376,13 @@ ALTER TABLE `tbl_list_of_service`
 -- AUTO_INCREMENT for table `tbl_payments`
 --
 ALTER TABLE `tbl_payments`
-  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `tbl_payment_logs`
 --
 ALTER TABLE `tbl_payment_logs`
-  MODIFY `logs_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `logs_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `tbl_role`
@@ -369,7 +400,7 @@ ALTER TABLE `tbl_sidebar`
 -- AUTO_INCREMENT for table `tbl_type_of_service`
 --
 ALTER TABLE `tbl_type_of_service`
-  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `tbl_user_info`
